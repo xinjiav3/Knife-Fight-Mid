c4b07ae50b785c54f2c2e07381b2d895dca1a7dc
EvanSvetina <evanthehedgehog43@gmail.com>
2025-05-02 13:31:38 -0700

    5001 port so i can use it

--- app.py
+++ app.py
@@ -184,5 +184,5 @@ def handle_attack(data):
         }, to=game_id)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     socketio.run(app, host='0.0.0.0', port=port, debug=True)
