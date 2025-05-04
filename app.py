b69ad8182e2ae06ee6147c94fcdbc0c9c90e0a96
Zhengji Li <127774804+xinjiav2@users.noreply.github.com>
2025-05-04 11:29:49 -0700

    ngrok

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, send_from_directory, request
 from flask_socketio import SocketIO, emit, join_room, leave_room
 import os
 import random
+import os
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 app.config['SECRET_KEY'] = 'your-secret-key'
---
+++ start_game.bat
@@ -0,0 +1,3 @@
+@echo off
+start ngrok.exe http 5001 --authtoken 2wYWcexX05qGnvjWAVWYyGncnki_3PmeyRmbWoX7R8meGuAg8
+python app.py
